f0afe60c728b7e36880b0b23b10e60bde5cc6f44
nawafalqari <nawafalqari13@gmail.com>
2023-07-04 03:17:42 +0300

    added error handling

--- client/comm/comm.py
+++ client/comm/comm.py
@@ -5,10 +5,16 @@ from ..config import read_config
 
 class Server:
     def __init__(self):
-        self.host = read_config()["server"]["host"]
+        self.host = read_config()["server"]["host"]        
 
     async def connect(self):
         try:
             self.server = await websockets.client.connect(self.host)
         except ConnectionRefusedError:
             messagebox.showerror(title="Error", message=f"Error connecting to server: {self.host}\nupdate the host in the settings menu")
+        except websockets.exceptions.InvalidURI:
+            messagebox.showerror(title="Error", message=f"Invalid host: {self.host}\nupdate the host in the settings menu")
+        except websockets.exceptions.InvalidStatusCode as err:
+            messagebox.showerror(title="Error", message=f"Invalid status code from server: {self.host} ({err.status_code})\nupdate the host in the settings menu")
+        except websockets.exceptions.InvalidHandshake:
+            messagebox.showerror(title="Error", message=f"Invalid handshake with server: {self.host}\nupdate the host in the settings menu")
