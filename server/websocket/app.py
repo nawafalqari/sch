c9179cdf3d11a7b4681205e5662174f9fd71be30
nawafalqari <nawafalqari13@gmail.com>
2023-07-09 16:56:57 +0300

    removed unwanted print

--- server/websocket/app.py
+++ server/websocket/app.py
@@ -8,7 +8,6 @@ def create_app(**data) -> FastAPI:
     async def server(ws: WebSocket):
         await ws.accept()
 
-        print(data)
         await connection_handler(ws, data)
 
     return app
