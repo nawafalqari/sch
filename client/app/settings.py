75f9296bb45c116fa1bfdb1d1070a6dce05fb09f
nawafalqari <nawafalqari13@gmail.com>
2023-07-13 10:21:42 +0300

    added notifications checkbox to settings

--- client/app/settings.py
+++ client/app/settings.py
@@ -11,7 +11,7 @@ class Settings(customtkinter.CTkToplevel):
         self.config = read_config()
 
         self.title(f"SCH - Settings ({self.config['server']['version']})")
-        self.geometry("300x250")
+        self.geometry("300x280")
         self.resizable(False, False)
         self.after(250, lambda: self.iconbitmap(self.config['client']['icon_path']))
 
@@ -29,6 +29,11 @@ class Settings(customtkinter.CTkToplevel):
         self.theme_select = customtkinter.CTkOptionMenu(self.settings_frame, width=200, height=40, corner_radius=10, values=["SCH", "Blue"], command=self.update_theme)
         self.theme_select.set(self.config["client"]["theme"])
         self.theme_select.pack(pady=5)
+
+        self.notifications_checkbox = customtkinter.CTkCheckBox(self.settings_frame, text="Receive notifications", onvalue="True", offvalue="False")
+        if read_config()["client"]["notifications"] == "True":
+            self.notifications_checkbox.select()
+        self.notifications_checkbox.pack(pady=5)
 
         self.options_frame = customtkinter.CTkFrame(self.settings_frame, width=300, height=100, corner_radius=10, fg_color="transparent")
 
@@ -52,11 +57,12 @@ class Settings(customtkinter.CTkToplevel):
             return
 
         update_config(server={
-            "host": self.host_input.get()
-        },
-        client={
-            "theme": self.theme_select.get(),
-            "nickname": self.nickname_input.get() if self.nickname_input.get() else ""
+                "host": self.host_input.get()
+            },
+            client={
+                "theme": self.theme_select.get(),
+                "nickname": self.nickname_input.get() if self.nickname_input.get() else "",
+                "notifications": self.notifications_checkbox.get()
         })
 
         self.destroy()
